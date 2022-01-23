f229149e5e0770458b85f5bdb5662f366bc693da
Suran Warnakulasooriya <68828123+suranwarnakulasooriya@users.noreply.github.com>
2022-01-23 14:21:19 -0500

    Delete panda.py

--- panda.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-d = pd.DataFrame([['a', 'b'], ['c', 'd']],
-                   index=['row 1', 'row 2'],
-                   columns=['col 1', 'col 2'])
-d.to_excel("output.xlsx")
